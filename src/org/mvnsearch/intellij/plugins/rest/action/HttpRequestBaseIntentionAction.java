73bd6e7e25a3081748a178ae626cfd092483377c
linux_china <linux_china@hotmail.com>
2018-01-17 09:50:33 +0800

    add getMethodSignature

--- src/org/mvnsearch/intellij/plugins/rest/action/HttpRequestBaseIntentionAction.java
+++ src/org/mvnsearch/intellij/plugins/rest/action/HttpRequestBaseIntentionAction.java
@@ -9,6 +9,8 @@ import org.mvnsearch.intellij.plugins.rest.HttpCall;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * http request base intention action
@@ -47,8 +49,28 @@ public abstract class HttpRequestBaseIntentionAction extends PsiElementBaseInten
         virtualFile.setBinaryContent(content.getBytes());
     }
 
-    protected String generateSeeRefer(PsiMethod psiMethod) {
-        return "@see #" + psiMethod.getName();
+    /**
+     * get method signature
+     *
+     * @param psiMethod         psi method
+     * @param includeParameters include parameters
+     * @return signature
+     */
+    protected String getMethodSignature(PsiMethod psiMethod, boolean includeParameters) {
+        //methodName(param_type, param_type)
+        StringBuilder builder = new StringBuilder();
+        builder.append(psiMethod.getName());
+        if (includeParameters) {
+            builder.append("(");
+            //params type
+            String paramsType = Stream.of(psiMethod.getParameterList().getParameters())
+                    .map(param -> param.getType().getCanonicalText()).collect(Collectors.joining(","));
+            if (paramsType != null) {
+                builder.append(paramsType);
+            }
+            builder.append(")");
+        }
+        return builder.toString();
     }
 
     /**
@@ -59,11 +81,12 @@ public abstract class HttpRequestBaseIntentionAction extends PsiElementBaseInten
      * @param isDefault     is default value
      * @return attribute value
      */
+    @SuppressWarnings("SameParameterValue")
     protected String getAttributeValue(PsiAnnotation psiAnnotation, String attributeName, boolean isDefault) {
         if (psiAnnotation == null) return "";
         String attributeValue = "";
         PsiNameValuePair[] attributes = psiAnnotation.getParameterList().getAttributes();
-        if (attributes.length == 1 && isDefault) {
+        if (attributes.length == 1) {
             PsiNameValuePair attribute = attributes[0];
             if (attributeName.equals(attribute.getName()) || (attribute.getName() == null && isDefault)) {
                 attributeValue = attribute.getLiteralValue();
