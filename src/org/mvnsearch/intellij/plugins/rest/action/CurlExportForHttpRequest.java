44c46aa47c051b53e05d8dcbb8e0847d51bbde65
linux_china <linux_china@hotmail.com>
2018-02-11 18:20:52 +0800

    add notification

--- src/org/mvnsearch/intellij/plugins/rest/action/CurlExportForHttpRequest.java
+++ src/org/mvnsearch/intellij/plugins/rest/action/CurlExportForHttpRequest.java
@@ -1,9 +1,14 @@
 package org.mvnsearch.intellij.plugins.rest.action;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.ws.http.request.HttpRequestVariableSubstitutor;
@@ -13,6 +18,9 @@ import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.datatransfer.StringSelection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -44,6 +52,7 @@ public class CurlExportForHttpRequest extends PsiElementBaseIntentionAction {
             HttpMethod httpMethod = (HttpMethod) parent;
             String curlCommand = generateCurlCommand((HttpRequest) httpMethod.getParent());
             CopyPasteManager.getInstance().setContents(new StringSelection(curlCommand));
+            Notifications.Bus.notify(new Notification("Http Request", "curl export", curlCommand, NotificationType.INFORMATION), project);
         }
     }
 
@@ -62,6 +71,7 @@ public class CurlExportForHttpRequest extends PsiElementBaseIntentionAction {
      */
     public String generateCurlCommand(HttpRequest httpRequest) {
         Project project = httpRequest.getProject();
+        Map<String, String> restClientEnv = getRestClientEnv(project);
         HttpRequestVariableSubstitutor substitutor = HttpRequestVariableSubstitutor.getDefault(project);
         StringBuilder builder = new StringBuilder("curl -v");
         //method
@@ -70,17 +80,58 @@ public class CurlExportForHttpRequest extends PsiElementBaseIntentionAction {
         //header
         builder.append(
                 httpRequest.getHeaderFieldList().stream()
-                        .map(headerField -> " -H '" + headerField.getName() + ": " + headerField.getValue(substitutor) + "'")
+                        .map(headerField -> " -H '" + headerField.getName() + ": " + replaceByEnv(restClientEnv, headerField.getValue(substitutor)) + "'")
                         .collect(Collectors.joining(" "))
         );
         //body
         if ("POST".equalsIgnoreCase(method) || "PUT".equalsIgnoreCase(method)) {
-            if (httpRequest.getRequestBody() != null && httpRequest.getRequestBody().getText() != null) {
-                builder.append(" -d '" + httpRequest.getRequestBody().getText().replaceAll(System.lineSeparator(), "\\\\" + System.lineSeparator()) + "'");
+            if (httpRequest.getRequestBody() != null) {
+                String requestBody = httpRequest.getRequestBody().getText();
+                if (requestBody != null && !requestBody.isEmpty()) {
+                    builder.append(" -d '" + replaceByEnv(restClientEnv, requestBody).replaceAll(System.lineSeparator(), "\\\\" + System.lineSeparator()) + "'");
+                }
             }
         }
         String httpUrl = httpRequest.getHttpUrl(substitutor);
-        builder.append(" " + httpUrl);
+        builder.append(" " + replaceByEnv(restClientEnv, httpUrl));
+        return builder.toString();
+    }
+
+    public Map<String, String> getRestClientEnv(Project project) {
+        Map<String, String> env = new HashMap<>();
+        try {
+            VirtualFile envJsonFile = project.getBaseDir().findChild("rest-client.env.json");
+            if (envJsonFile != null) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map allEnv = objectMapper.readValue(envJsonFile.getInputStream(), LinkedHashMap.class);
+                if (!allEnv.isEmpty()) {
+                    Map.Entry entry = (Map.Entry) allEnv.entrySet().iterator().next();
+                    env = (Map<String, String>) entry.getValue();
+                }
+            }
+        } catch (Exception ignore) {
+
+        }
+        return env;
+    }
+
+    public String replaceByEnv(Map<String, String> restClientEnv, String text) {
+        StringBuilder builder = new StringBuilder();
+        String keyword = "{{";
+        int start = 0;
+        int offset = 0;
+        while ((offset = text.indexOf(keyword, start)) > 0) {
+            builder.append(text, start, offset);
+            Integer end = text.indexOf("}}", offset + keyword.length());
+            String var = text.substring(offset + keyword.length(), end);
+            if (restClientEnv.containsKey(var)) {
+                builder.append(restClientEnv.get(var));
+            } else {
+                builder.append("{{" + var + "}}");
+            }
+            start = end + 2;
+        }
+        builder.append(text, start, text.length());
         return builder.toString();
     }
 
